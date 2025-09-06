7367d975f045784436102ce063e372f5228e6b29
rerruu <darell.liebert@gmail.com>
2025-09-06 15:22:06 +0700

    change api url [am]

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://tes-amazink.test/api',
+  baseURL: 'https://test-amazink-aurel.rplrus.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
